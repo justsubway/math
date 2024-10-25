1a1536cdf5d67ba626e5fdcaed131289568bf5fd
Subway <128174770+justsubway@users.noreply.github.com>
2024-10-25 09:47:03 +0300

    Update settings.js
    animation for collapsible settings menu

--- settings.js
+++ settings.js
@@ -27,3 +27,21 @@ window.onload = function () {
     });
     }
 };
+
+//animation
+
+document.addEventListener('DOMContentLoaded', () => {
+    const collapsibles = document.getElementsByClassName("collapsible");
+
+    for (let i = 0; i < collapsibles.length; i++) {
+        collapsibles[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            const content = this.nextElementSibling;
+            if (content.style.maxHeight) {
+                content.style.maxHeight = null;
+            } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+            }
+        });
+    }
+});
